e99102b08acfaa60c26ffff808792314a9e6b674
Henry Zongaro <zongaro@ca.ibm.com>
2021-05-10 14:12:17 -0400

    Added environment variables for various experiments:
    1) In IL generation to disable use of helpers for value types for ACMP, AASTORE and AALOAD - TR_Disable{ACMP|AALOAD|AASTORE}HelperIlGen
    
    2) In Tree Lowerig to Allow ACMP to be transformed unconditionally to a reference comparison only, with no null tests or checks for value type - TR_ACMPFastestPathOnly
    
    3) In Value Propagation to force VP to think ACMP, AALOAD or AASTORE operations do not involve value types - TR_{ACMP|AALOAD|AASTORE}ForceVPXform

--- runtime/compiler/optimizer/J9ValuePropagation.cpp
+++ runtime/compiler/optimizer/J9ValuePropagation.cpp
@@ -673,6 +673,8 @@ J9::ValuePropagation::constrainRecognizedMethod(TR::Node *node)
          return;
          }
 
+      static char *forceACMPVPXform = feGetEnv("TR_ACMPForceVPXform");
+
       bool lhsGlobal, rhsGlobal;
       TR::Node *lhsNode = node->getChild(0);
       TR::Node *rhsNode = node->getChild(1);
@@ -688,7 +690,7 @@ J9::ValuePropagation::constrainRecognizedMethod(TR::Node *node)
       // is definitely not an instance of a value type or if both operands
       // are definitely references to the same object
       //
-      if (isLhsValue == TR_no || isRhsValue == TR_no || areSameRef)
+      if (isLhsValue == TR_no || isRhsValue == TR_no || areSameRef || forceACMPVPXform)
          {
          if (performTransformation(
                comp(),
@@ -763,6 +765,9 @@ J9::ValuePropagation::constrainRecognizedMethod(TR::Node *node)
          return;
          }
 
+      static char *forceAALOADVPXform = feGetEnv("TR_AALOADForceVPXform");
+      static char *forceAASTORREVPXform = feGetEnv("TR_AASTORREForceVPXform");
+
       bool arrayRefGlobal;
       bool storeValueGlobal;
       const int storeValueOpIndex = isLoadFlattenableArrayElement ? -1 : 0;
@@ -774,6 +779,13 @@ J9::ValuePropagation::constrainRecognizedMethod(TR::Node *node)
       TR::Node *storeValueNode = isStoreFlattenableArrayElement ? node->getChild(storeValueOpIndex) : NULL;
       TR::VPConstraint *arrayConstraint = getConstraint(arrayRefNode, arrayRefGlobal);
       TR_YesNoMaybe isCompTypeVT = isArrayCompTypeValueType(arrayConstraint);
+
+      // Should we force VP to think that array operations never involve value types?
+      if (forceAALOADVPXform && isLoadFlattenableArrayElement
+          || forceAASTORREVPXform && isStoreFlattenableArrayElement)
+         {
+         isCompTypeVT = TR_no;
+         }
 
       static char *disableCheckStoreElementValueIsVT = feGetEnv("TR_DisableCheckStoreElementValueIsVT");
       TR_YesNoMaybe isStoreValueVT = (!disableCheckStoreElementValueIsVT && isStoreFlattenableArrayElement) ? isValue(getConstraint(storeValueNode, storeValueGlobal)) : TR_maybe;
